af9afcbffc916a0c1768dcb0a16e2211a3580292
Ваше Имя <raznomazov_vm@mail.ru>
2020-03-01 22:02:33 +0200

    SQL12.py was changed

--- SQL12.py
+++ SQL12.py
@@ -7,3 +7,4 @@ sql6="select* from The_equipments_of_magnetic_pulse_forming"
 sql7='''Insert into The_equipments_of_magnetic_pulse_forming values (?,?,?,?,?,?,?);'''
 sql8="Delete from The_equipments_of_magnetic_pulse_forming where Equipment_brand =?"
 sql9='''Update The_equipments_of_magnetic_pulse_forming set Equipment_brand =?,Max_charge_energy =?,Condenser_capasity =?,Equipment_inductance =?,Shot_circuit_current_frequency =?,FK1 =?,FK2 =? where Equipment_brand =?'''
+sql10="select* from Workpiece_material"
