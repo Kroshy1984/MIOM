3bca1626ccb4b39f9655a8d858791e050b58c0f3
Ваше Имя <raznomazov_vm@mail.ru>
2020-03-01 18:31:36 +0200

    OperationSwitch.py was changed

--- OperationSwitch.py
+++ OperationSwitch.py
@@ -2,23 +2,29 @@ from tkinter import *
 class OperationSwitch():
     def __init__(self):
         self.OperationSwitch = Tk()
-        self.OperationSwitch.geometry('500x200')  # геометрия окна
+        self.OperationSwitch.geometry('500x300')  # геометрия окна
         self.OperationSwitch.title("Выбор операции")  # название окна
         lbl = Label(self.OperationSwitch, text="Выберите необходимую операцию:", font=("Arial Bold", 20))
         lbl.grid(column=0, row=0)
         selected = IntVar()
-        rad1 = Radiobutton(self.OperationSwitch, text='Формовка цилиндра, конуса, сферы', value=1, variable=selected)
-        rad2 = Radiobutton(self.OperationSwitch, text='Формовка рифтов', value=2, variable=selected)
-        rad3 = Radiobutton(self.OperationSwitch, text='Гибка бортов', value=3, variable=selected)
-        rad4 = Radiobutton(self.OperationSwitch, text='Формовка плоских заготовок', value=4, variable=selected)
+        rad1 = Radiobutton(self.OperationSwitch, text='Свободная раздача трубчатой заготовки', value=1, variable=selected)
+        rad2 = Radiobutton(self.OperationSwitch, text='Формовка переходников', value=2, variable=selected)
+        rad3 = Radiobutton(self.OperationSwitch, text='Формовка рифтов', value=3, variable=selected)
+        rad4 = Radiobutton(self.OperationSwitch, text='Разделительные операции на трубах', value=4, variable=selected)
+        rad5 = Radiobutton(self.OperationSwitch, text='Пробивка отверстий в плоских заготовках', value=5, variable=selected)
+        rad6 = Radiobutton(self.OperationSwitch, text='Гибка бортов', value=6, variable=selected)
+        rad7 = Radiobutton(self.OperationSwitch, text='Формовка плоских заготовок', value=7, variable=selected)
         rad1.place(x=10, y=40)
         rad2.place(x=10, y=60)
         rad3.place(x=10, y=80)
         rad4.place(x=10, y=100)
+        rad5.place(x=10, y=120)
+        rad6.place(x=10, y=140)
+        rad7.place(x=10, y=160)
         btn = Button(self.OperationSwitch , text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
-        btn.place(x=10, y=150)
+        btn.place(x=10, y=250)
         btn = Button(self.OperationSwitch, text="Внести данные", bg="green", fg="black", command=self.Insertdata)
-        btn.place(x=300, y=150)
+        btn.place(x=300, y=250)
         self.OperationSwitch.mainloop()
 
     def Insertdata(self): pass
