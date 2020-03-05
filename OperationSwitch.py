5b3137f4c5b737bdf12842eebb805c3ad2dcffcb
Ваше Имя <raznomazov_vm@mail.ru>
2020-03-05 11:04:44 +0200

    OperationSwitch.py was changed. RadioButtons was updated with loop

--- OperationSwitch.py
+++ OperationSwitch.py
@@ -26,38 +26,23 @@ class OperationSwitch():
 
     def GUI (self):
         self.OperationSwitch = Tk()
-        self.OperationSwitch.geometry('500x300')  # геометрия окна
+        self.OperationSwitch.geometry('500x500')  # геометрия окна
         self.OperationSwitch.title("Выбор операции")  # название окна
         lbl = Label(self.OperationSwitch, text="Выберите необходимую операцию:", font=("Arial Bold", 20))
         lbl.grid(column=0, row=0)
         self.selected = IntVar()
-        self.selected.set(10)
-        rad1 = Radiobutton(self.OperationSwitch, text='Свободная раздача трубчатой заготовки', variable=self.selected, value=1,command=self.GoToWork)
-        rad2 = Radiobutton(self.OperationSwitch, text='Формовка переходников', variable=self.selected,value=2, command=self.GoToWork)
-        rad3 = Radiobutton(self.OperationSwitch, text='Формовка рифтов',  variable=self.selected, value=3, command=self.GoToWork)
-        rad4 = Radiobutton(self.OperationSwitch, text='Разделительные операции на трубах', variable=self.selected,value=4, command=self.GoToWork)
-        rad5 = Radiobutton(self.OperationSwitch, text='Пробивка отверстий в плоских заготовках', variable=self.selected,value=5, command =self.GoToWork)
-        rad6 = Radiobutton(self.OperationSwitch, text='Гибка бортов', variable=self.selected, value=6, command = self.GoToWork)
-        rad7 = Radiobutton(self.OperationSwitch, text='Формовка плоских заготовок', variable=self.selected,value=7, command=self.GoToWork)
-        rad8 = Radiobutton(self.OperationSwitch, text='Калибровка на отжим', variable=self.selected,value=8, command=self.GoToWork)
-        rad9 = Radiobutton(self.OperationSwitch, text='Калибровка на раздачу', variable=self.selected,value=9, command=self.GoToWork)
-        rad1.place(x=10, y=40)
-        rad2.place(x=10, y=60)
-        rad3.place(x=10, y=80)
-        rad4.place(x=10, y=100)
-        rad5.place(x=10, y=120)
-        rad6.place(x=10, y=140)
-        rad7.place(x=10, y=160)
-        rad8.place(x=10, y=180)
-        rad9.place(x=10, y=200)
+        self.selected.set(3)
+        operations=[('Свободная раздача трубчатой заготовки',1),('Формовка переходников',2),('Формовка рифтов',3),('Разделительные операции на трубах',4),('Пробивка отверстий в плоских заготовках',5),('Гибка бортов',6),('Формовка плоских заготовок',7),('Калибровка на отжим',8),('Калибровка на раздачу',9)]
+        for operation,val in operations:
+            Radiobutton(self.OperationSwitch, text=operation, variable=self.selected, value=val).place(x=10, y=val*35)
         btn = Button(self.OperationSwitch, text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
-        btn.place(x=10, y=250)
-        btn = Button(self.OperationSwitch, text="Внести данные", bg="green", fg="black", command=self.Insertdata)
-        btn.place(x=300, y=250)
+        btn.place(x=10, y=350)
+        btn = Button(self.OperationSwitch, text="Внести данные", bg="green", fg="black", command=self.GoToWork)
+        btn.place(x=300, y=350)
+        self.OperationSwitch.mainloop()
     def __init__(self):
         self.GUI()
-        self.OperationSwitch.mainloop()
+        print(self.selected.get())
 
-    def Insertdata(self): pass
 
     def CloseWindow(self): self.OperationSwitch.destroy()
