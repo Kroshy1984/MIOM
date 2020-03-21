65a011529877b6aa6ef687360ee0d326bda8ddf8
Ваше Имя <raznomazov_vm@mail.ru>
2020-03-21 15:34:24 +0200

    InductorParams.py was changed

--- InductorParams.py
+++ InductorParams.py
@@ -1,15 +1,16 @@
 from tkinter import *
 class InductorParams():
-    def __init__(self):
+    def GUI(self):
         self.InductorParams = Tk()
         self.InductorParams.geometry('400x200')  # геометрия окна
         self.InductorParams.title("Параметры индуктора")  # название окна
-        btn = Button(self.InductorParams , text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
+        btn = Button(self.InductorParams, text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
         btn.place(x=10, y=150)
         btn = Button(self.InductorParams, text="Внести данные", bg="green", fg="black", command=self.Insertdata)
         btn.place(x=200, y=150)
         self.InductorParams.mainloop()
-
+    def __init__(self):
+        self.GUI()
     def Insertdata(self): pass
 
     def CloseWindow(self): self.InductorParams.destroy()
