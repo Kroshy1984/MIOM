a25d4899743dab27de3e2d24a220fdd1cd95637a
Ваше Имя <raznomazov_vm@mail.ru>
2020-03-29 07:46:41 +0200

    Materials.py was changed. Button was changed. Place and size were changed. Labels are cutted

--- Materials.py
+++ Materials.py
@@ -7,7 +7,7 @@ from EditorMaterials import *
 class Materials():
     def GUI(self):
         self.Materials = Tk()
-        self.Materials.geometry('1800x1000')
+        self.Materials.geometry('1350x700')
         self.Materials.title("Выбор материала ")
         self.Tree = ttk.Treeview(self.Materials, columns=(
             "Name_of_the_metalls", "Tensile_strength", "Yield_strength", "Material_density", "M_M", "B",
@@ -32,68 +32,21 @@ class Materials():
         self.Tree.heading("Specific_electric_resistance", text="Удельное Сопротивление")
         self.Tree.heading("The_coefficient_of_dynamic", text="Динамический коэффициент")
         self.Tree.heading("Еhe_dynamic_modulus_hardening", text="Модуль коэффициента жесткости")
-        self.Tree.place(x=400, y=10)
-        self.slct2 = ['Выбери из таблицы', 'Выбери из таблицы', 'Выбери из таблицы', 'Выбери из таблицы',
-                      'Выбери из таблицы', 'Выбери из таблицы', 'Выбери из таблицы']
-        self.field1 = self.slct2[0]
-        self.field2 = self.slct2[1]
-        self.field3 = self.slct2[2]
-        self.field4 = self.slct2[3]
-        self.field5 = self.slct2[4]
-        self.field6 = self.slct2[5]
-        self.field7 = self.slct2[6]
+        self.Tree.place(x=50, y=10)
         self.btn = Button(self.Materials, text="Добавить материал", bg="grey", fg="black",
                           command=self.AddMaterial)  # описание объекта типа button названия кнопки
-        self.btn.place(x=250, y=800)  # расположение кнопки
+        self.btn.place(x=150, y=650)  # расположение кнопки
         self.btn5 = Button(self.Materials, text="Редактировать материал", bg="grey", fg="black",
                            command=self.EditMaterial)  # описание объекта типа button названия кнопки
-        self.btn5.place(x=750, y=800)  # расположение кнопки
+        self.btn5.place(x=650, y=650)  # расположение кнопки
         self.btn2 = Button(self.Materials, text="Удалить материал", bg="grey", fg="black",
                            command=self.DelMaterial)  # описание объекта типа button названия кнопки
-        self.btn2.place(x=500, y=800)  # расположение кнопки
+        self.btn2.place(x=400, y=650)  # расположение кнопки
         self.btn3 = Button(self.Materials, text="Применить", bg="grey",
                            fg="black")  # описание объекта типа button названия кнопки
-        self.btn3.place(x=1000, y=800)  # расположение кнопки
+        self.btn3.place(x=900, y=650)  # расположение кнопки
         self.btn1 = Button(self.Materials, text="Отменить", bg='pink', fg='red')
-        self.btn1.place(x=1250, y=800)
-        self.btn4 = Button(self.Materials, text="Взять данные в работу", bg='green', fg='black')
-        self.btn4.place(x=50, y=800)
-        if self.field1 == 'Выбери из таблицы': self.btn4['state'] = 'disabled'
-        label1 = Label(self.Materials, text="Металл", bg="grey", fg="black")
-        self.message_entry = Entry(self.Materials, textvariable='')
-        self.message_entry.insert(0, self.field1)
-        self.message_entry.place(x=50, y=75)
-        label2 = Label(self.Materials, text="Предел прочности", bg="grey", fg="black")
-        self.message_entry1 = Entry(self.Materials, textvariable='')
-        self.message_entry1.place(x=50, y=175)
-        self.message_entry1.insert(0, self.field2)
-        label3 = Label(self.Materials, text="Предел текучести", bg="grey", fg="black")
-        self.message_entry2 = Entry(self.Materials, textvariable='')
-        self.message_entry2.place(x=50, y=275)
-        self.message_entry2.insert(0, self.field3)
-        label4 = Label(self.Materials, text="Плотность", bg="grey", fg="black")
-        self.message_entry3 = Entry(self.Materials, textvariable='')
-        self.message_entry3.place(x=50, y=375)
-        self.message_entry3.insert(0, self.field4)
-        label5 = Label(self.Materials, text="М_м", bg="grey", fg="black")
-        self.message_entry4 = Entry(self.Materials, textvariable='')
-        self.message_entry4.place(x=50, y=475)
-        self.message_entry4.insert(0, self.field5)
-        label6 = Label(self.Materials, text="В", bg="grey", fg="black")
-        self.message_entry5 = Entry(self.Materials, textvariable='')
-        self.message_entry5.place(x=50, y=575)
-        self.message_entry5.insert(0, self.field6)
-        label7 = Label(self.Materials, text="Предел прочности", bg="grey", fg="black")
-        self.message_entry6 = Entry(self.Materials, textvariable=self.field7)
-        self.message_entry6.place(x=50, y=675)
-        self.message_entry6.insert(0, self.field7)
-        label1.place(x=50, y=50)
-        label2.place(x=50, y=150)
-        label3.place(x=50, y=250)
-        label4.place(x=50, y=350)
-        label5.place(x=50, y=450)
-        label6.place(x=50, y=550)
-        label7.place(x=50, y=650)
+        self.btn1.place(x=1150, y=650)
         self.view_records()
         self.Materials.mainloop()
     def __init__(self,a,b,c,d):
