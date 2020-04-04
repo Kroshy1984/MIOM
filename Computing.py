212675c67a46c416fc51b84282f3afed7d255951
Ваше Имя <raznomazov_vm@mail.ru>
2020-04-04 15:18:36 +0200

    Computing.py was changed. Inductor was changed. BP was addicted

--- Computing.py
+++ Computing.py
@@ -2,19 +2,22 @@
 #Название операции operation
 #Диаметр наружной трубы DOT
 #Толщина стенки трубы ST
+#астота разрядного тока FW
+#Глубина проникновения ИМП в материал заготовки BP
 class Inductor():
-    def __init__(self, LBT, operation, DOT, ST):
+    def __init__(self,LBT,operation,DOT,ST,FW,YEMP,mu):
         self.LBT=LBT
         self.operation=operation
         self.DOT=DOT
         self.ST=ST
-        print(self.LBT, self.operation)
         self.ZS=0.65 # Толщина изоляции медной шины
         self.ZB=1.0 # Толщина основной изоляции индуктора
         self.ZA=0.25 #Толщина воздушного зазора
         self.LTC=0.7*pow(10,-7)#Индуктивность токов индуктора
         self.ZCP = self.ZS + self.ZB + self.ZA
         self.DCA = self.DOT - 2 * self.ST - 2 * self.ZCP
+        self.FW=FW#Частота разрядного тока
+        self.BP = pow((YEMP / (3.14 * mu * FW),0.5)#Глубина проникновения ИМП в материал заготовки
     def LCA(self):
         if self.operation=="a1":self.LCA=1.1*self.LBT #формовка цилиндра
         elif self.operation=="a2":self.LCA=1.3*self.LBT#формовка конуса
@@ -25,6 +28,10 @@ class Inductor():
         return self.ZSP
     def DCA(self):
         return self.DCA
+    def BP(self):#Глубина проникновения ИМП в материал заготовки
+        return self.BP
+
+
 #Диаметр наружной трубы DOT
 #Толщина стенки трубы ST
 #Коэффициент динамичности материала KDM
