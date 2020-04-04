e3ba8c34ad7efdca493a7f8229c282be569bb4da
Ваше Имя <raznomazov_vm@mail.ru>
2020-04-04 16:06:37 +0200

    Computing.py was changed. Inductor was changed. BC was addicted

--- Computing.py
+++ Computing.py
@@ -4,8 +4,11 @@
 #Толщина стенки трубы ST
 #астота разрядного тока FW
 #Глубина проникновения ИМП в материал заготовки BP
+#обозначение удельного электрического сопротивления заготовки YEMP
+#обозначение удельного электрического сопротивления материала индуктора YEMC
 class Inductor():
-    def __init__(self,LBT,operation,DOT,ST,FW,YEMP,mu):
+    def __init__(self,LBT,operation,DOT,ST,FW,YEMP,YEMC):
+        mu = 4 * 3.17 * pow(10, -7)# магнитная проницаемость в вакууме
         self.LBT=LBT
         self.operation=operation
         self.DOT=DOT
@@ -14,10 +17,13 @@ class Inductor():
         self.ZB=1.0 # Толщина основной изоляции индуктора
         self.ZA=0.25 #Толщина воздушного зазора
         self.LTC=0.7*pow(10,-7)#Индуктивность токов индуктора
+        self.YEMP=YEMP#обозначение удельного электрического сопротивления заготовки
+        self.YEMC=YEMC
         self.ZCP = self.ZS + self.ZB + self.ZA
         self.DCA = self.DOT - 2 * self.ST - 2 * self.ZCP
         self.FW=FW#Частота разрядного тока
-        self.BP = pow((YEMP / (3.14 * mu * FW),0.5)#Глубина проникновения ИМП в материал заготовки
+        self.BC = pow((self.YEMC / (3.14 * mu *self.FW),0.5))#Глубина проникновения ИМП в материал индуктор
+        self.BP=pow((self.YEMP / (3.14 * mu * self.FW),0.5))#Глубина проникновения ИМП в материал заготовки
     def LCA(self):
         if self.operation=="a1":self.LCA=1.1*self.LBT #формовка цилиндра
         elif self.operation=="a2":self.LCA=1.3*self.LBT#формовка конуса
@@ -25,11 +31,13 @@ class Inductor():
         elif self.operation=="a4":self.LCA=0.75*self.LBT#формовка рифта
         return self.LCA
     def ZCP(self):#Величина зазора между индуктором и заготовкой
-        return self.ZSP
+        return self.ZСP
     def DCA(self):
         return self.DCA
     def BP(self):#Глубина проникновения ИМП в материал заготовки
         return self.BP
+    def BC(self):#Глубина проникновения ИМП в материал индуктор
+        return self.BC
 
 
 #Диаметр наружной трубы DOT
