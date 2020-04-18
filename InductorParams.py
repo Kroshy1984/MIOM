6f9f3bcf32f08930af9a2b1578cd9090f5f6cde4
Ваше Имя <raznomazov_vm@mail.ru>
2020-04-18 15:21:23 +0200

    InductorParams.py was changed. Buttons and Labels were addicted

--- InductorParams.py
+++ InductorParams.py
@@ -2,15 +2,30 @@ from tkinter import *
 class InductorParams():
     def GUI(self):
         self.InductorParams = Tk()
-        self.InductorParams.geometry('400x200')  # геометрия окна
+        self.InductorParams.geometry('500x300')  # геометрия окна
         self.InductorParams.title("Параметры индуктора")  # название окна
-        btn = Button(self.InductorParams, text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
-        btn.place(x=10, y=150)
-        btn = Button(self.InductorParams, text="Внести данные", bg="green", fg="black", command=self.Insertdata)
-        btn.place(x=200, y=150)
+        label1 = Label(self.InductorParams, text="Диаметр наружной трубы", bg="white", fg="black")
+        label1.place(x=10, y=10)
+        label2 = Label(self.InductorParams, text="Толщина стенки", bg="white", fg="black")
+        label2.place(x=10, y=60)
+        label3 = Label(self.InductorParams, text="Диаметр деформированной зоны", bg="white", fg="black")
+        label3.place(x=10, y=110)
+        btn1 = Button(self.InductorParams, text="Выбор оборудования", bg="grey", fg="black", command=self.WindowMashins)
+        btn1.place(x=10,y=170)
+        self.message_entry = Entry(self.InductorParams, textvariable='')
+        self.message_entry.place(x=250, y=10)
+        self.message_entry1 = Entry(self.InductorParams, textvariable='')
+        self.message_entry1.place(x=250, y=60)
+        self.message_entry2 = Entry(self.InductorParams, textvariable='')
+        self.message_entry2.place(x=250, y=110)
+        btn2 = Button(self.InductorParams, text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
+        btn2.place(x=10, y=250)
+        btn3 = Button(self.InductorParams, text="Внести данные", bg="green", fg="black", command=self.Insertdata)
+        btn3.place(x=350, y=250)
         self.InductorParams.mainloop()
     def __init__(self):
         self.GUI()
     def Insertdata(self): pass
 
     def CloseWindow(self): self.InductorParams.destroy()
+    def WindowMashins(self):pass
--- SelectWay.py
+++ SelectWay.py
@@ -19,7 +19,7 @@ class SelectWay():
         btn2.place(x=10, y=11)
         btn3 = Button(frame_bottom, text="Назад", bg="red", fg="black", command=self.Exit)
         btn3.place(x=500, y=11)
-        btn1 = Button(frame_bottom, text="Расчет Индуктор", bg="grey", fg="black", command=self.InductorWindow)
+        btn1 = Button(frame_bottom, text="Расчет Индуктора", bg="grey", fg="black", command=self.InductorWindow)
         btn1.place(x=300, y=11)
         label1 = Label(frame_top, text="Выберете путь для рассчета процессов МИОМ", bg="white", fg="black")
         label1.pack()
