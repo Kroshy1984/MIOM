3d0659c4dff193b43ab72b2498b0e6f880863ca4
Ваше Имя <raznomazov_vm@mail.ru>
2020-04-18 15:50:50 +0200

    InductorParams.py was changed. Buttons and Labels were changed

--- InductorParams.py
+++ InductorParams.py
@@ -2,7 +2,7 @@ from tkinter import *
 class InductorParams():
     def GUI(self):
         self.InductorParams = Tk()
-        self.InductorParams.geometry('500x300')  # геометрия окна
+        self.InductorParams.geometry('500x400')  # геометрия окна
         self.InductorParams.title("Параметры индуктора")  # название окна
         label1 = Label(self.InductorParams, text="Диаметр наружной трубы", bg="white", fg="black")
         label1.place(x=10, y=10)
@@ -11,7 +11,11 @@ class InductorParams():
         label3 = Label(self.InductorParams, text="Диаметр деформированной зоны", bg="white", fg="black")
         label3.place(x=10, y=110)
         btn1 = Button(self.InductorParams, text="Выбор оборудования", bg="grey", fg="black", command=self.WindowMashins)
-        btn1.place(x=10,y=170)
+        btn1.place(x=10, y=170)
+        btn4 = Button(self.InductorParams, text="Выбор материала", bg="grey", fg="black", command=self.WindowMashins)
+        btn4.place(x=10, y=220)
+        btn5 = Button(self.InductorParams, text="Выбор операции", bg="grey", fg="black", command=self.WindowMashins)
+        btn5.place(x=10, y=280)
         self.message_entry = Entry(self.InductorParams, textvariable='')
         self.message_entry.place(x=250, y=10)
         self.message_entry1 = Entry(self.InductorParams, textvariable='')
@@ -19,13 +23,12 @@ class InductorParams():
         self.message_entry2 = Entry(self.InductorParams, textvariable='')
         self.message_entry2.place(x=250, y=110)
         btn2 = Button(self.InductorParams, text="Закрыть окно", bg="red", fg="black", command=self.CloseWindow)
-        btn2.place(x=10, y=250)
-        btn3 = Button(self.InductorParams, text="Внести данные", bg="green", fg="black", command=self.Insertdata)
-        btn3.place(x=350, y=250)
+        btn2.place(x=10, y=350)
+        btn3 = Button(self.InductorParams, text="Рассчитать", bg="green", fg="black", command=self.Insertdata)
+        btn3.place(x=350, y=350)
         self.InductorParams.mainloop()
     def __init__(self):
         self.GUI()
     def Insertdata(self): pass
-
     def CloseWindow(self): self.InductorParams.destroy()
     def WindowMashins(self):pass
