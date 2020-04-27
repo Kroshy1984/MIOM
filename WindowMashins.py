34816e2e18a890dd836e4ca7ba56594206420a8e
Ваше Имя <raznomazov_vm@mail.ru>
2020-04-27 21:42:03 +0200

    WindowMashins.py was changed. Labels are addicted.

--- WindowMashins.py
+++ WindowMashins.py
@@ -1,5 +1,7 @@
-from tkinter import ttk
+from tkinter import *
 import sqlite3
+from tkinter.ttk import Treeview
+
 from SQL12 import *
 import EntranceData
 import Terminator
@@ -8,20 +10,23 @@ import EditorMashins
 
 class Basad():
     def GUI(self):
-        self.BasaM2 = EditorMashins.Tk()
+        self.BasaM2 = Tk()
         self.BasaM2.geometry('1330x700')
         self.BasaM2.title("Выбор оборудования МИОМ")
-        self.Tree = EditorMashins.ttk.Treeview(self.BasaM2, columns=(
+        self.Tree = Treeview(self.BasaM2, columns=(
             "Name", "Max_change_energi", "Condenser_capasity", "Equipment_induct", "SccF", "FK1","FK2","R0"), height=20,
                                                show='headings')
-        self.Tree.column("Name", width=250, anchor=EditorMashins.tk.CENTER)
-        self.Tree.column("Max_change_energi", width=190, anchor=EditorMashins.tk.CENTER)
-        self.Tree.column("Condenser_capasity", width=50, anchor=EditorMashins.tk.CENTER)
-        self.Tree.column("Equipment_induct", width=50, anchor=EditorMashins.tk.CENTER)
-        self.Tree.column("SccF", width=50, anchor=EditorMashins.tk.CENTER)
+        self.Tree.column("Name", width=150, anchor=CENTER)
+        self.Tree.column("Max_change_energi", width=70, anchor=CENTER)
+        self.Tree.column("Condenser_capasity", width=50, anchor=CENTER)
+        self.Tree.column("Equipment_induct", width=50, anchor=CENTER)
+        self.Tree.column("SccF", width=50, anchor=CENTER)
+        self.Tree.column("FK1", width=50, anchor=CENTER)
+        self.Tree.column("FK2", width=50, anchor=CENTER)
+        self.Tree.column("R0", width=50, anchor=CENTER)
         self.Tree['show'] = "headings"
-        self.Tree.heading("Name", text="Наименование оборудования")
-        self.Tree.heading("Max_change_energi", text="Максимальная энергия")
+        self.Tree.heading("Name", text="Наименование")
+        self.Tree.heading("Max_change_energi", text="W_mash")
         self.Tree.heading("Condenser_capasity", text="CCE")
         self.Tree.heading("Equipment_induct", text="LCE")
         self.Tree.heading("SccF", text="FW")
@@ -29,6 +34,20 @@ class Basad():
         self.Tree.heading("FK2", text=" FK2")
         self.Tree.heading("R0", text="R0")
         self.Tree.place(x=50, y=10)
+        label1 = Label(self.BasaM2, text="W_mash-максимальная мощность разряда", bg="lightgrey", fg="black")
+        label1.place(x=600, y=30)
+        label2=Label(self.BasaM2, text="CCT-Емкость батареи конденсаторов установки", bg="lightgrey", fg="black")
+        label2.place(x=600,y=50)
+        label3 = Label(self.BasaM2, text="LCE-индуктивность", bg="lightgrey", fg="black")
+        label3.place(x=600, y=70)
+        label4 = Label(self.BasaM2, text="FW-величина тока короткого замыкания", bg="lightgrey", fg="black")
+        label4.place(x=600, y=90)
+        label5 = Label(self.BasaM2, text="FK1-коэффициент", bg="lightgrey", fg="black")
+        label5.place(x=600, y=110)
+        label6 = Label(self.BasaM2, text="FK2-коэффициент", bg="lightgrey", fg="black")
+        label6.place(x=600, y=130)
+        label7 = Label(self.BasaM2, text="R0-сопротивление", bg="lightgrey", fg="black")
+        label7.place(x=600, y=150)
         self.btn = EditorMashins.Button(self.BasaM2, text="Добавить оборудование", bg="grey", fg="black",
                                         command=self.AddMashins)  # описание объекта типа button названия кнопки
         self.btn.place(x=300, y=500)  # расположение кнопки
