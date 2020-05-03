25de5c5cc5ce9e4120b95932ca73ac8f4d0f2808
Ваше Имя <raznomazov_vm@mail.ru>
2020-05-03 19:12:31 +0200

    WindowMashins.py was changed. Edit Toplevel Window was created.

--- WindowMashins.py
+++ WindowMashins.py
@@ -32,7 +32,7 @@ class Basad():
         self.Tree.place(x=50, y=10)
         label1 = Label(self.BasaM2, text="W_mash-максимальная мощность разряда", bg="lightgrey", fg="black")
         label1.place(x=510, y=30)
-        label2=Label(self.BasaM2, text="CCT-Емкость батареи конденсаторов установки", bg="lightgrey", fg="black")
+        label2=Label(self.BasaM2, text="CCЕ-Емкость батареи конденсаторов установки", bg="lightgrey", fg="black")
         label2.place(x=510,y=50)
         label3 = Label(self.BasaM2, text="LCE-индуктивность", bg="lightgrey", fg="black")
         label3.place(x=510, y=70)
@@ -46,7 +46,7 @@ class Basad():
                                         command=self.AddMashins)  # описание объекта типа button названия кнопки
         btn.place(x=510, y=200)  # расположение кнопки
         btn5 = Button(self.BasaM2, text="Редактировать оборудование", bg="orange", fg="black",
-                                         command=self.clicked2)  # описание объекта типа button названия кнопки
+                                         command=self.Edit)  # описание объекта типа button названия кнопки
         btn5.place(x=510, y=250)  # расположение кнопки
         btn2 = Button(self.BasaM2, text="Удалить оборудование", bg="orange", fg="black",
                                          command=self.DellMashins)  # описание объекта типа button названия кнопки
@@ -55,15 +55,60 @@ class Basad():
         btn1.place(x=510, y=350)
 
 
-    def clicked2(self):
+    def Edit(self):
         sel = self.Tree.focus()
-        self.slct2 = self.Tree.item(sel, option='values')
-        self.field1 = self.slct2[0]
-        self.field2 = self.slct2[1]
-        self.field3 = self.slct2[2]
-        self.field4 = self.slct2[3]
-        self.field5 = self.slct2[4]
-        NewWindow = EditorMashins.Editor(self.field1, self.field2, self.field3, self.field4, self.field5)
+        slct2 = self.Tree.item(sel, option='values')
+        field1 = slct2[0]
+        field2 = slct2[1]
+        field3 = slct2[2]
+        field4 = slct2[3]
+        field5 = slct2[4]
+        field6 = slct2[5]
+        field7 = slct2[4]
+        self.Editor = Toplevel(self.BasaM2)
+        self.Editor.geometry('350x400+1200+400')
+        self.Editor.title("Редактировать Материал")
+        self.Editor.lift()
+        self.Editor.attributes('-topmost', True)
+        label1 = Label(self.Editor, text="Наименование", bg="lightgrey", fg="black")
+        self.message_entry = Entry(self.Editor, textvariable="")
+        self.message_entry.place(x=150, y=10)
+        self.message_entry.insert(0,str(field1))
+        label2 = Label(self.Editor, text="W_mash", bg="lightgrey", fg="black")
+        self.message_entry1 = Entry(self.Editor, textvariable="")
+        self.message_entry1.place(x=150, y=60)
+        self.message_entry1.insert(0, str(field2))
+        label3 = Label(self.Editor, text="CCE", bg="lightgrey", fg="black")
+        self.message_entry2 = Entry(self.Editor, textvariable="")
+        self.message_entry2.place(x=150, y=110)
+        self.message_entry2.insert(0, str(field3))
+        label4 = Label(self.Editor, text="LCE", bg="lightgrey", fg="black")
+        self.message_entry3 = Entry(self.Editor, textvariable="")
+        self.message_entry3.place(x=150, y=160)
+        self.message_entry3.insert(0, str(field4))
+        label5 = Label(self.Editor, text="FCE", bg="lightgrey", fg="black")
+        self.message_entry4 = Entry(self.Editor, textvariable="")
+        self.message_entry4.place(x=150, y=210)
+        self.message_entry4.insert(0, str(field5))
+        label6 = Label(self.Editor, text="R0", bg="lightgrey", fg="black")
+        self.message_entry5 = Entry(self.Editor, textvariable="")
+        self.message_entry5.place(x=150, y=260)
+        self.message_entry5.insert(0, str(field6))
+        label7 = Label(self.Editor, text="Fw", bg="lightgrey", fg="black")
+        self.message_entry6 = Entry(self.Editor, textvariable="")
+        self.message_entry6.place(x=150, y=310)
+        self.message_entry6.insert(0, str(field7))
+        label1.place(x=10, y=10)
+        label2.place(x=10, y=60)
+        label3.place(x=10, y=110)
+        label4.place(x=10, y=160)
+        label5.place(x=10, y=210)
+        label6.place(x=10, y=260)
+        label7.place(x=10, y=310)
+        btn1 = Button(self.Editor, text="Закрыть", bg='red', fg='black', command=self.Editor.destroy)
+        btn1.place(x=30, y=350)
+        btn2 = Button(self.Editor, text="Запомнить", bg='lightgreen', fg='black')
+        btn2.place(x=200, y=350)
 
     def AddMashins(self):
         self.Editor = Toplevel(self.BasaM2)
