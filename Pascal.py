32f3bcd43580f69389f212a82d774e2b2b3a2a4f
Ваше Имя <raznomazov_vm@mail.ru>
2020-05-17 11:59:20 +0200

    Pascal.py was changed

--- Pascal.py
+++ Pascal.py
@@ -1,5 +1,5 @@
 import math
-def Ston(io,vb,ka,vg,Ln,q0,dd,bb,pc,i2,Gamma0,Gamma1,Gamma3,i4):
+def Ston(io,vb,ka,vg,q0,dd,bb,pc,i2,Gamma0,Gamma1,Gamma3,i4):
     f=[]
     y=[]
     if (io == 1):
@@ -12,7 +12,7 @@ def Ston(io,vb,ka,vg,Ln,q0,dd,bb,pc,i2,Gamma0,Gamma1,Gamma3,i4):
             y[1]=0
         else:
             f[0]=y[1]
-            ss1 =(4.0/3.0-vb/3.0)*ka*vg*Ln(1+vg*y[1])/(1.0+vg*y[1])
+            ss1 =((4.0/3.0-vb/3.0)*ka*vg*math.log(1+vg*y[1])/(1.0+vg*y[1]),math.exp(1))
             ss2 =(vb+(1.0-vb)*q0)*dd/(1.0+vg*y[1])
             ss3 =bb*pc*(vb*math.sqrt(1.0+vg*y[1])+(1.0-vb)*(1.0+vg*y[1]))
             f[2]=ss3-ss1-ss2
@@ -40,7 +40,21 @@ def zub3(N_Y,Time_h,f,k,y,w):
         k[j] = k[j] + vk * 2.0
         y[j] = w[j] + vk
 
-Ston(io,vb,ka,vg,Ln,q0,dd,bb,pc,i2,Gamma0,Gamma1,Gamma3,i4)
+io=1
+vb=1
+ka= 4 * ey * lw * c0 /(pm*dh*dh)
+vg=-1
+q0 = 2.0 / math.pow(3.0, 0.5)
+dd = 4 * sp * lw * c0 / pm / dh / dh
+bb = mu0 * (c0 * U0 * n1 * kn) * (c0 * U0 * n1 * kn) / (pm * dh * h0 * l0 * l0)
+pc = 0.5*((vg-1)*(2.0*i2+i4)*i4+(vg+1)*i4*i4)*(zaz/(zaz+kappa*S_tek/1000))
+i2 = ( y[4]*alfa3-y[5]*m13 ) / ( (1+alfa1)*alfa3-m13*m13 )
+Gamma0 = R0 * math.pow((c0 / lw), 0.5)
+Gamma1 = Rind * math.pow((c0 / lw), 0.5)
+Gamma3 = Rzag * n1 * n1 * math.pow((c0 / lw), 0.5)
+i4 = (y[4]*m13-y[5]*(1+alfa1))/( (1+alfa1)*alfa3-m13*m13 )*(-1.0)
+
+Ston(io,vb,ka,vg,q0,dd,bb,pc,i2,Gamma0,Gamma1,Gamma3,i4)
 zub1(N_Y,Time_h,f,w)
 Time_x = Time_x + Time_h/2.0
 Ston(io,vb,ka,vg,Ln,q0,dd,bb,pc,i2,Gamma0,Gamma1,Gamma3,i4)
