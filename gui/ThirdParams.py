5aad1b39e78bfe935b90e3f6584659fdae551328
DrRipley <fedotova.sonya@gmail.com>
2020-06-09 00:22:13 +0300

    Change QTableWidget to QTableView

--- gui/ThirdParams.py
+++
@@ -1,9 +0,0 @@
-
-from PyQt5.QtWidgets import QWidget
-from PyQt5.uic import loadUi
-
-class ThirdParams(QWidget):
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-        loadUi('./gui/ThirdParams.ui', self)
-        print("3")
