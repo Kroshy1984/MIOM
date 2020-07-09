4c8bf029fbbac48fce28b93e66454c2ea16df297
Ваше Имя <raznomazov_vm@mail.ru>
2020-07-09 22:04:29 +0300

    InitialParameters.py was changed. Dictinary was created.

--- gui/InitialParameters.py
+++ gui/InitialParameters.py
@@ -180,7 +180,7 @@ class InitialParameters(QWidget):
                                float(self.YEMC), float(self.LTC))
         print(g)
         self.make_file(g,a)
-        params = {"WR" : g.WR, "I00": g.I00,"FP":g.FP, "DZT":g.DZT}
+        params = {"WR" : g.WR, "I00": g.I00,"FP":g.FP, "DZT":g.DZT, "KEC": g.KEC, "EPS":a.EPS}
         print(params)
         self._parent.secondary_parameters._show(True, params)
 
